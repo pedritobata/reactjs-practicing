d6775fd8d8d7dc9cfbc373a51dbcf2bbd576f6e2
unknown <pedromartinez_goyo@hotmail.com>
2019-09-16 17:11:41 -0500

    lifecycle methods

--- personas-basico1/src/components/Persons/Persons.js
+++ personas-basico1/src/components/Persons/Persons.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import Person from './Person2/Person2';
 
-class Persons extends React.Component{
+class Persons extends React.PureComponent{
+
+    state = {
+        nuevoRenderizado: ''
+    }
+
+    constructor(props){
+        super(props);
+        this.state.nuevoRenderizado = 'No renderizado nuevo';
+        console.log('[Persons.js] constructor()');
+    }
+
+
+    static getDerivedStateFromProps(props, state){
+        console.log('[Persons.js] getDerivedStateFromProps()');
+
+        return state;
+    }
+
+    /*
+    shouldComponentUpdate(nextProps,nextState){
+        console.log('[Persons.js] shouldComponentUpdate()');
+        if(nextProps.persons !== this.props.persons){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    */
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('[Persons.js] getSnapshotBeforeUpdate()');
+        return {message: 'Snapshot!'};
+    }
+
+    componentDidUpdate(prevProps,prevState,snapshot){
+        // OJO OJO invocar setState en este metodo causa un bucle infinito!!
+        //usar una condicion para que se invoque setState
+        if(this.state.nuevoRenderizado !== prevState.nuevoRenderizado){
+            this.setState({nuevoRenderizado : 'renderizar nuevamente'});
+        }
+        
+        console.log('[Persons.js] componentDidUpdate()');
+        console.log(snapshot);
+    }
+
+
 
 
     render(){
 
+        console.log('[Persons.js] render()');
 
         return this.props.persons.map((person, index) => {
             return (
